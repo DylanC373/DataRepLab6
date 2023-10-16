7ae6416408259906e64cfd35f51a24ba34cf56f8
unknown <Coynoh@outlook.com>
2023-10-16 14:38:56 +0000

    Week 5 Lab 4

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,31 +1,34 @@
 import { useEffect, useState } from 'react';
 import Books from './Books'
 import axios from "axios";
-function Read(){     
+function Read() {
 
-   const [data, setData] = useState ([]);
+    const [data, setData] = useState([]);
 
-    useEffect(()=>{
-
+    // UseEffect is a react hook that will allow you to sync your component with an external system
+    useEffect(() => {
+        //Axios will make a http request that has the get method for the following URL 
+        axios.get('https://jsonblob.com/api/jsonblob/1161593332966481920')
+            .then( //This is the call back function when the asynchronous function is ocmpleted this is function that is executed 
+                (response) => {
+                    //setData will update the value of data with the response that came back from the server 
+                    setData(response.data.books)
+                }
+            )
+            .catch(
+                (error) => {
+                    console.log(error);
+                }
+            );
     }, []);
-    axios.get('https://jsonblob.com/api/jsonblob/1161593332966481920')
-    .then(
-        (response)=>{
-            setData(response.data.books)
-        }
-    )
-    .catch(
-        (error)=>{
-            console.log(error);
-        }
-    );
-    return(
-<div>
-    <h2>Hello from my read component</h2>
-    <Books myBooks = {data}>
-        
-    </Books>
-</div>
+
+    return (
+        <div>
+            <h2>Hello from my read component</h2>
+            <Books myBooks={data}>
+
+            </Books>
+        </div>
     );
 }
 export default Read; 
